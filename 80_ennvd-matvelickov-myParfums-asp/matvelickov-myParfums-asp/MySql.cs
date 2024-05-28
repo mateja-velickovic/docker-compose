c894112c440b2d8d9571b1381d15d37a50186358
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-05-28 14:36:53 +0200

    :large_blue_diamond: feat(docker): Rendu du projet
    + Environnement de développement Docker
    + Application myParfums
    + Base de données SQL
    + phpMyAdmin
    + Dockerfile
    + docker-compose.yml

---
+++ 80_ennvd-matvelickov-myParfums-asp/matvelickov-myParfums-asp/MySql.cs
@@ -0,0 +1,50 @@
+﻿/*using MySql.Data.MySqlClient;
+using System;
+
+namespace matvelickov_myParfums_asp
+{
+    public class MySql
+    {
+        private static string connString = "server=localhost;port=3306;uid=root;pwd=root;database=db_parfums";
+
+        public static void MySqlConn()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    conn.Open();
+                    Console.WriteLine("Connection successful.");
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error connecting to MySQL: " + ex.Message);
+            }
+        }
+
+        public static string GetParfumName()
+        {
+            /*try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connString))
+                {
+                    connection.Open();
+                    string query = "SELECT parNom FROM t_parfum";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        object result = command.ExecuteScalar();
+                        return result != null ? result.ToString() : null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+        return "mateja";
+    }
+}
+*/
---
+++ 80_ennvd-matvelickov-myParfums-asp/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ajout du package MySql
+# dotnet add package Pomelo.EntityFrameworkCore.MySql
+
+# Exécuter docker compose
+docker-compose up 
